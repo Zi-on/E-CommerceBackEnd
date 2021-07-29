e698f79c9ac995b918184ac321ded7f60b2109bc
Zi-on <zioniflores12@yahoo.com>
2021-07-29 18:19:24 -0500

    added delete request for products route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -112,8 +112,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const singleProduct = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!singleCategory) {
+      res.status(404).json({ message: 'No Product with this id exists'});
+      return;
+    }
+    res.status(200).json(singleProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
