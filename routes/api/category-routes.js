f6aa2e6dc50368ff91a4e49d9ba95a76bca3d9f9
Zi-on <zioniflores12@yahoo.com>
2021-07-29 17:15:41 -0500

    added post request to my category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,9 +37,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const singleCategory = await Category.create({
-        Category_id: req.body.Category_id,
-       });
+    const singleCategory = await Category.create(req.body);
       res.status(200).json(singleCategory);
   } catch (err) {
     res.status(500).json(err);
