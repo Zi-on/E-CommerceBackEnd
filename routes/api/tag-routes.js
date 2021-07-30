bf8faa496b5d2320e8a598b9922f33b9e605ca80
Zi-on <zioniflores12@yahoo.com>
2021-07-29 22:53:26 -0500

    created request to get a request to get single tag based on id, created post request and working on put request

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,18 +16,50 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  
+  try {
+    const singleTag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'tag_id'}],
+    })
+  if (!singleTag){
+    res.status(404).json({ message: 'Tag associated with this id does not exists'});
+    return;
+  }
+  res.status(200).json(singleTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const singleTag = await Tag.create(req.body);
+    res.status(200).json(singleTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const singleTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      },
+    });
+    if (!singleCategory) {
+      res.status(404).json({ message: 'No tag with this id exists.'});
+      return;
+    }
+    res.status(200).json(singleTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
